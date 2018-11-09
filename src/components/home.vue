3832637cae40bc56b956d221a32b6a56f05d7f50
Joshua Strzeszkowski <joshstrzeszkowski@gmail.com>
2018-11-09 12:43:23 -0600

    home page displaying games and images

--- src/components/home.vue
+++ src/components/home.vue
@@ -18,52 +18,52 @@ export default {
       games: [
         {
           title: 'The Last of Us',
-          image: '',
+          image: require('../assets/the_last_of_us_cover.png'),
           yearOfRelease: 2013
         },
         {
           title: 'Metal Gear Solid',
-          image: '',
+          image: require('../assets/metal_gear_solid_art.jpg'),
           yearOfRelease: 1998
         },
         {
           title: 'The Legend of Zelda: Ocarina of Time',
-          image: '',
+          image: require('../assets/The Legend of Zelda_ Ocarina of Time_cover.jpg'),
           yearOfRelease: 1998
         },
         {
           title: 'Grand Theft Auto V',
-          image: '',
+          image: require('../assets/gtav_cover.jpg'),
           yearOfRelease: 2013
         },
         {
           title: 'Max Payne',
-          image: '',
+          image: require('../assets/max_payne_art.jpg'),
           yearOfRelease: 2001
         },
         {
           title: 'Resident Evil',
-          image: '',
+          image: require('../assets/220px-Resident_Evil_2002_cover.jpg'),
           yearOfRelease: 2002
         },
         {
           title: 'Halo: Combat Evolved',
-          image: '',
+          image: require('../assets/halo_cover.jpg'),
           yearOfRelease: 2001
         },
         {
           title: 'God of War',
-          image: '',
+          image: require('../assets/god_of_war_ps2.png'),
           yearOfRelease: 2005
         },
         {
           title: 'Uncharted 4: A Thief\'s End',
-          image: '',
+          image: require('../assets/uncharted4_cover.jpg'),
           yearOfRelease: 2016
         },
         {
           title: 'Prince of Persia: The Sands of Time',
-          image: '',
+          image: require('../assets/220px-Sands_of_time_cover.jpg'),
           yearOfRelease: 2003
         },
       ]
